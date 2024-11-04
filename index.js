7b0d7b517b85a0f7fa9c2c3bb86829e8d3fe1111
norma13115 <normapadilla13115@gmail.com>
2024-11-04 03:06:29 +0000

    added styles css js html

---
+++ index.js
@@ -0,0 +1,26 @@
+const storeName = 'Pretty Baby Store';
+const storeType = 'Baby Store';
+const product1 = 'Baby Food';
+const product2 = 'Baby Clothes';
+const product3 = 'Baby Toys';
+const product4 = 'Baby Shoes';
+const product5 = 'Baby Accessories';
+
+
+
+const price1 =  15.99;
+const price2 =  10.99; 
+const price3 =  20.99;
+const price4 =  5.99;
+const price5 =  8.99;
+
+
+const storeHours = "Monday - Friday: 9 AM - 6 PM, Saturday: 10 AM - 4 PM, Sunday: Closed";
+
+console.log(storeName, storeType);
+console.log(product1, price1);
+console.log(product2, price2);
+console.log(product3, price3);
+console.log(product4, price4);
+console.log(product5, price5);
+console.log(storeHours);
