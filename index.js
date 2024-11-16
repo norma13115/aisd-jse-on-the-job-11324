0a13f221505e82f6fb7eb45fa4d5c69d23f77dfe
norma13115 <normapadilla13115@gmail.com>
2024-11-15 19:54:48 -0800

    added new js code

--- index.js
+++ index.js
@@ -24,3 +24,28 @@ console.log(product3, price3);
 console.log(product4, price4);
 console.log(product5, price5);
 console.log(storeHours);
+
+// Build arrays from the product and price variables
+let products = [product1, product2, product3, product4, product5];
+let prices = [price1, price2, price3, price4, price5];
+
+console.log(`Welcome to ${storeName}! We are the best ${storeType} in town.`);
+console.log("Here are the products we currently offer:");
+
+// Display the initial product list
+console.log("Products:", products);
+console.log("Prices:", prices);
+// Example of push: Add a new product to the store
+products.push("baby clothes");
+prices.push(5.99);
+console.log("We've added a new product! Products:", products);
+console.log("Updated Prices:", prices);
+
+// Example of pop: Remove the last product from the store
+products.pop();
+prices.pop();
+console.log("Unfortunately, Shampoo is out of stock. Products:", products);
+console.log("Updated Prices:", prices);
+
+console.log(`Thanks for visiting ${storeName}`);
+console.log(`Here are our store hours for reference: ${storeHours}`);
